1f2b2352b95c5a32fd6c28d01b07a2e92a0c9fe3
scicluna <88948700+scicluna@users.noreply.github.com>
2023-02-03 15:10:18 -0500

    Completed initial class frameworks and redesigned the file framework

---
+++ index.js
@@ -0,0 +1,12 @@
+//Dependencies
+const inquirer = require(inquirer)
+const fs = require(fs)
+
+//Class imports
+const Employee = require("./lib/Employee")
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+
+
+
