d7ae81eabcd864b579cf5a39a67f732f16a05625
scicluna <88948700+scicluna@users.noreply.github.com>
2023-02-03 18:11:38 -0500

    All basic functionality complete

--- README.md
+++ README.md
@@ -5,20 +5,24 @@ THEN an HTML file is generated that displays a nicely formatted team roster base
 WHEN I click on an email address in the HTML
 THEN my default email program opens and populates the TO field of the email with the address
 
-WHEN I click on the GitHub username
-THEN that GitHub profile opens in a new tab
+WHEN I click on the GitHub username - DONE
+THEN that GitHub profile opens in a new tab - DONE
 
-WHEN I start the application
-THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+WHEN I start the application - DONE
+THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number - DONE
 
-WHEN I enter the team manager’s name, employee ID, email address, and office number
-THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+WHEN I enter the team manager’s name, employee ID, email address, and office number - DONE
+THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team - DONE
 
-WHEN I select the engineer option
-THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+WHEN I select the engineer option - DONE
+THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu - DONE
 
-WHEN I select the intern option
-THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+WHEN I select the intern option - DONE
+THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu - DONE
 
-WHEN I decide to finish building my team
-THEN I exit the application, and the HTML is generated
+WHEN I decide to finish building my team - DONE
+THEN I exit the application, and the HTML is generated - DONE
+
+VIDEO TUTORIAL
+
+JEST TESTS
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Dependencies
-const inquirer = require(inquirer)
-const fs = require(fs)
+const inquirer = require("inquirer")
+const fs = require("fs")
 
 //Class imports
 const Employee = require("./lib/Employee")
@@ -8,5 +8,227 @@ const Manager = require("./lib/Manager")
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the name of this team's manager?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is their employee ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is their email address?"
+    },
+    {
+        type: "input",
+        name: "office",
+        message: "What is their office number?"
+    },
+]
 
+const pickEngineervsIntern = [
+    {
+        type: "list",
+        name: "type",
+        message: "Would you like to add an Engineer or Intern?",
+        choices: ["Engineer", "Intern", "Neither"]
+    }
+]
 
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is their ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is their email?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is their github username?"
+    },
+]
+
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is their ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is their email?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school do they go to"
+    },
+]
+
+const team = []
+
+function init(){
+    managerPrompts(managerQuestions)
+}
+init()
+
+function managerPrompts(questions){
+    inquirer.prompt(questions)
+    .then(data=>{
+        generateManager(data)
+        engineerOrIntern(pickEngineervsIntern)
+    })
+}
+
+function engineerOrIntern(questions){
+    inquirer.prompt(questions)
+    .then(data=>{
+        if (data.type === "Engineer") engineerPrompts(engineerQuestions)
+        if (data.type === "Intern") internPrompts(internQuestions)
+        if (data.type === "Neither") {
+            generateHTML(team)
+        }
+    })
+}
+
+function engineerPrompts(questions){
+    inquirer.prompt(questions)
+    .then(data=>{
+        generateEngineer(data)
+        engineerOrIntern(pickEngineervsIntern)
+    })
+}
+
+function internPrompts(questions){
+    inquirer.prompt(questions)
+    .then(data=>{
+        generateIntern(data)
+        engineerOrIntern(pickEngineervsIntern)
+    })
+}
+
+function generateManager({name, id, email, office}){
+    const manager = new Manager(name, id, email, office)
+    team.push(manager)
+}
+
+function generateEngineer({name, id, email, github}){
+    const engineer = new Engineer(name, id, email, github)
+    team.push(engineer)
+}
+
+function generateIntern({name, id, email, school}){
+    const intern = new Intern(name, id, email, school)
+    team.push(intern)
+}
+
+function generateHTML(team){
+    const html = writeHTML(team)
+    writeToFile("index.html", html)
+}
+
+function writeHTML(team){
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+      crossorigin="anonymous" />
+    <title>Team Members</title>
+  </head>
+  <body>
+    <div class="d-flex flex-column vh-100">
+        <nav class="navbar bg-body-tertiary container-fluid d-flex justify-content-center bg-danger-subtle">
+            <h1>My Team</h1>
+        </nav>
+        <main class="d-flex p-2 flex-wrap align-content-center justify-content-center bg-warning-subtle h-100">
+            ${buildCards(team)}
+        </main>
+    </div>
+    </body>
+  </html>
+    `
+}
+
+function writeToFile(fileName, content){
+    fs.writeFile(fileName, content, (err) =>
+    err ? console.log(err) : console.log(`Successfully created ${fileName}`)
+)}
+
+function buildCards(team){
+    const cards = team.map(member=>{
+        let special;
+        let specialDesc;
+        let specialTag;
+        let specialLink = ''
+        let specialStyle = ''
+        let specialTarget = ''
+        if (member.getRole() == "Manager") {
+            special = member.officeNumber
+            specialTag = "li"
+            specialDesc = "Office Number:"
+        }
+        if (member.getRole() == "Engineer") { 
+            special = member.getGithub()
+            specialTag = "a"
+            specialDesc = "Github:"
+            specialLink = `github.com/${special}`
+            specialStyle = 'link-primary'
+            specialTarget = '_blank'
+        }
+        if (member.getRole() == "Intern") { 
+            special = member.getSchool()
+            specialTag = "li"
+            specialDesc = "School:"
+
+        }
+
+        return `
+        <div class="card m-2" style="width: 16rem;">
+            <div class="card-body">
+                <h5 class="card-title">${member.getName()}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${member.getRole()}</h6>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${member.getId()}</li>
+                    <a href="mailto:${member.getEmail()}"  class="list-group-item">Email: <span class="link-primary">${member.getEmail()}</span></a>
+                    <${specialTag} href="${specialLink}" target="${specialTarget}" class="list-group-item">${specialDesc} <span class=${specialStyle}>${special}</span></${specialTag}>
+                </ul>
+            </div>
+        </div>
+        `
+    })
+    return cards.join("")
+}
+
+//PLAN: Sketch an HTML page in prototype
+//Use Bootstrap to style it so that we dont need a css page to make it look good
+//Grid size based on team size? How Am I going to do that? 
+//How to make it dynamic to the team size and actually listing out details from the whole team?
+//Like, I really wish I was using react now, because I feel like this would be easy.
